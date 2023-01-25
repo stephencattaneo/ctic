20a99619a1d19dd33b5c15de9d38c1fea17f42dd
Stephen Cattaneo <stephen.cattaneo@gmail.com>
2023-01-24 19:48:26 -0800

    linkedlist: add delete and length

--- linkedlist/list.py
+++ linkedlist/list.py
@@ -6,18 +6,54 @@ from .node import Node, DoubleNode
 class List:
     head = None
 
+    def __init__(self, args: list = None ) -> None:
+        if args:
+            for i in args:
+                self.append(i)
+
     def append(self, data: Any) -> None:
         if not self.head:
             self.head = Node(data)
+            return
 
-        else:
-            curr = self.head
-            while curr.next:
-                curr = curr.next
-            curr.next = Node(data)
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = Node(data)
 
-class DoubleList:
-    head = None
+    def __len__(self):
+        count = 0
+        curr = self.head
+
+        while curr:
+            curr = curr.next
+            count += 1
+
+        return count
+
+    def delete(self, value: Any) -> bool:
+        if not self.head:
+            return False
+
+        if self.head.data == value:
+            self.head = self.head.next
+            return True
+
+        prev = self.head
+        curr = self.head.next
+
+        while curr:
+            if curr.data == value:
+                prev.next = curr.next
+                return True
+            
+            prev = curr
+            curr = curr.next
+
+        return False
+
+
+class DoubleList(List):
     tail = None
 
     def append(self, data: Any) -> None:
@@ -27,3 +63,26 @@ class DoubleList:
             prevtail = self.tail
             self.tail = DoubleNode(data, self.tail)
             prevtail.next = self.tail
+
+    def delete(self, value: Any) -> bool:
+        if not self.head:
+            return False
+
+        if self.head.data == value:
+            self.head = self.head.next
+            self.head.previous = None
+            return True
+
+        prev = self.head
+        curr = self.head.next
+
+        while curr:
+            if curr.data == value:
+                prev.next = curr.next
+                curr.next.previous = prev
+                return True
+            
+            prev = curr
+            curr = curr.next
+
+        return False
--- linkedlist/test_linkedlist.py
+++ linkedlist/test_linkedlist.py
@@ -1,44 +1,68 @@
 from .list import List, DoubleList
 
-def test_emptylist_append():
-    lut = List()
-    assert lut.head is None
 
-    lut.append(5)
-    assert lut.head.data == 5
-    assert lut.head.next is None
+class TestSingleLink:
+    def test_emptylist_append(self):
+        lut = List()
+        assert lut.head is None
 
-def test_singlelist_multiple_items():
-    lut = List()
+        lut.append(5)
+        assert lut.head.data == 5
+        assert lut.head.next is None
 
-    lut.append(6)
-    lut.append(3)
+    def test_append_multiple_items(self):
+        lut = List()
 
-    assert lut.head.data == 6
-    assert lut.head.next.data == 3
+        lut.append(6)
+        lut.append(3)
 
-def test_emptydouble_append():
-    lut = DoubleList()
-    assert lut.head is None
-    assert lut.tail is None
+        assert lut.head.data == 6
+        assert lut.head.next.data == 3
 
-    lut.append(9)
-    assert lut.head == lut.tail
-    assert lut.head.data == 9
+    def test_init_append(self):
+        lut = List(range(5))
 
-def test_doublelist_multiple_items():
-    lut = DoubleList()
+        assert lut.head.data == 0
+        assert lut.head.next.data == 1
 
-    lut.append(342)
-    lut.append(65)
-    assert lut.head.data == 342
-    assert lut.tail.data == 65
-    assert lut.head.next == lut.tail
-    assert lut.tail.previous == lut.head
+    def test_len(self):
+        lut = List(range(2))
 
-    lut.append(94)
-    assert lut.head.data == 342
-    assert lut.tail.data == 94
-    assert lut.head.next.data == 65
-    assert lut.head.next.next.data == 94
+        assert len(lut) == 2
+
+    def test_len_empty(self):
+        assert len(List()) == 0
+
+    def test_delete(self):
+        lut = List(range(3))
+
+        lut.delete(1)
+
+        assert lut.head.next.data == 2
+
+class TestDoubleLink:
+    def test_empty_append(self):
+        lut = DoubleList()
+        assert lut.head is None
+        assert lut.tail is None
+
+        lut.append(9)
+        assert lut.head == lut.tail
+        assert lut.head.data == 9
+
+    def test_append_multiple_items(self):
+        lut = DoubleList()
+
+        lut.append(342)
+        lut.append(65)
+        assert lut.head.data == 342
+        assert lut.tail.data == 65
+        assert lut.head.next == lut.tail
+        assert lut.tail.previous == lut.head
+
+        lut.append(94)
+        assert lut.head.data == 342
+        assert lut.tail.data == 94
+        assert lut.head.next.data == 65
+        assert lut.head.next.next.data == 94
 
